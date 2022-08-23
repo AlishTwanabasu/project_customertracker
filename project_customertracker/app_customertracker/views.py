782fa6c53cf524cb35d561494e0940ed414a1032
AlishTwanbasu <alishtwanabasu5@gmail.com>
2022-08-23 14:55:44 +0545

    project & app initialized

---
+++ project_customertracker/app_customertracker/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# task of this views.py file
+# 1. render template
+# 2. send request from template to model
+# 3. verify and validate requests
+# 4. send response for requests from model to template
+
+# Create your views here.
